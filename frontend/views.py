18d56bdf8a055f3d77a3fb7c25fee3659b28e986
NephygTdkingz <tdkingzict@gmail.com>
2025-11-25 15:31:39 +0100

    initial commit

---
+++ frontend/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'frontend/index.html')
+
+def about(request):
+    return render(request, 'frontend/about.html')
