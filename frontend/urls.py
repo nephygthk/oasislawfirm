ab4081fb828de77f139c3efbd70f4f7ae6771724
NephygTdkingz <tdkingzict@gmail.com>
2025-11-25 16:35:46 +0100

    added team and contact page

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,4 +6,7 @@ app_name = 'frontend'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about-us', views.about, name='about'),
+    path('services', views.service, name='service'),
+    path('team', views.team, name='team'),
+    path('contact-us', views.contact, name='contact'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -5,3 +5,12 @@ def home(request):
 
 def about(request):
     return render(request, 'frontend/about.html')
+
+def service(request):
+    return render(request, 'frontend/service.html')
+
+def team(request):
+    return render(request, 'frontend/team.html')
+
+def contact(request):
+    return render(request, 'frontend/contact.html')
